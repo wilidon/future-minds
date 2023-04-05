e227b43f9d05e1fadb92d3207ab3a739bb3d91b2
Ildar <wilidon@bk.ru>
2023-04-05 23:43:15 +0400

    Поменял формат возврата курсов пользователя

--- src/main/java/ru/ildar/futureminds/controller/UserController.java
+++ src/main/java/ru/ildar/futureminds/controller/UserController.java
@@ -7,11 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.ildar.futureminds.dto.course.CourseMainDTO;
 import ru.ildar.futureminds.dto.user.ProfileRequest;
+import ru.ildar.futureminds.dto.user.UserProfileResponse;
 import ru.ildar.futureminds.exception.UserNotFound;
 import ru.ildar.futureminds.model.Course;
 import ru.ildar.futureminds.model.User;
+import ru.ildar.futureminds.model.UserCourse;
 import ru.ildar.futureminds.service.AuthService;
 import ru.ildar.futureminds.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -29,7 +34,13 @@ public class UserController {
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        List<CourseMainDTO> courseMainDTOS = new ArrayList<>();
+        for (UserCourse course : user.getCourses()) {
+            courseMainDTOS.add(convertToDTO(course.getCourse()));
+        }
+        UserProfileResponse userProfileResponse = convertToDTO(user);
+        userProfileResponse.setCourses(courseMainDTOS);
+        return new ResponseEntity<>(userProfileResponse, HttpStatus.OK);
     }
 
     @PutMapping("/")
@@ -41,6 +52,7 @@ public class UserController {
         catch (UserNotFound userNotFound) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
@@ -57,12 +69,27 @@ public class UserController {
 
     @GetMapping("/{user_id}/courses")
     public ResponseEntity<?> getAllCourseByUser_id(@PathVariable int user_id) {
-        return new ResponseEntity<>(HttpStatus.OK);
+        User user = userService.findById(user_id).orElse(null);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        List<CourseMainDTO> courseMainDTOS = new ArrayList<>();
+        for (UserCourse course : user.getCourses()) {
+            courseMainDTOS.add(convertToDTO(course.getCourse()));
+        }
+        return new ResponseEntity<>(courseMainDTOS, HttpStatus.OK);
     }
+
+
 
     private CourseMainDTO convertToDTO(Course course) {
         return modelMapper.map(course, CourseMainDTO.class);
     }
+
+    private UserProfileResponse convertToDTO(User user) {
+        return modelMapper.map(user, UserProfileResponse.class);
+    }
+
 
     private User convertToEntity(ProfileRequest profileRequest) {
         return modelMapper.map(profileRequest, User.class);
